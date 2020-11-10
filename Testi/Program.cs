507a5067da4419063407d48ab337ed3ded40b1ba
Neutriino84 <autti.mika84@gmail.com>
2020-11-10 16:25:53 +0200

    Add project files.

---
+++ Testi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kokeilu versio....
+        
+        
+        }
+    }
+}
